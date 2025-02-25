f1b3b496644efdc021a9385bad604a9b933e7043
joaocarloslima <joaocarloslima@me.com>
2025-02-25 20:49:35 -0300

    category: index, create, get

--- src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_flow_api/controller/CategoryController.java
@@ -1,30 +1,56 @@
 package br.com.fiap.money_flow_api.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.money_flow_api.model.Category;
 
-@RestController //component
+@RestController // component
 public class CategoryController {
 
-    //Listar todas as categorias
+    private List<Category> repository = new ArrayList<>();
+
+    // Listar todas as categorias
     // GET :8080/categories -> 200 ok -> json
     @GetMapping("/categories")
-    public List<Category> index(){
-        return List.of(
-            new Category(1L, "Educação", "book"),
-            new Category(2L, "Lazer", "casino"),
-            new Category(3L, "Salário", "payments")
-        );
+    public List<Category> index() {
+        return repository;
     }
 
-    //Cadastrar categorias
+    // Cadastrar categorias
+    @PostMapping("/categories")
+    // @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        System.out.println("Cadastrando..." + category.getName());
+        repository.add(category);
+        return ResponseEntity.status(201).body(category);
+    }
 
-    //Apagar categorias
-    
+    // Detalhes da categorias
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> get(@PathVariable Long id) {
+        System.out.println("Buscando categoria " + id);
+        var category = repository
+                .stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+
+        if (category.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(category.get());
+    }
+
+    // Apagar categorias
+
+    // Editar categorias
+
 }
--- src/main/java/br/com/fiap/money_flow_api/model/Category.java
+++ src/main/java/br/com/fiap/money_flow_api/model/Category.java
@@ -1,12 +1,14 @@
 package br.com.fiap.money_flow_api.model;
 
+import java.util.Random;
+
 public class Category {
     private Long id;
     private String name;
     private String icon;
 
     public Category(Long id, String name, String icon) {
-        this.id = id;
+        this.id = (id == null) ? new Random().nextLong() : id;
         this.name = name;
         this.icon = icon;
     }
@@ -23,7 +25,4 @@ public class Category {
         return icon;
     }
 
-    
-
-    
 }
